ff80287fb81c41c946ac8133c8adb132f9a199d2
Dheeraj <92251016+Dheeraj20042003@users.noreply.github.com>
2023-03-29 00:38:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+var num = document.querySelectorAll(".drum").length;
+var i=0;
+while(i<num)
+{
+
+    document.querySelectorAll("button")[i].addEventListener("click",function(){ 
+
+        var buttonchar  = this.innerHTML;
+      createsound(buttonchar);
+      crateanimation(buttonchar);
+      
+    });
+    i++;
+}
+
+document.addEventListener("keydown",function(event){
+
+    createsound(event.key);
+    crateanimation(event.key)
+})
+
+
+function createsound(key)
+{
+
+    switch(key)
+    {
+        case "w":
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+         case "j":
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+         case "k":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        default:
+            break;
+    }
+
+}
+
+function crateanimation(key)
+{
+    
+    var currkey = document.querySelector("."+ key);
+     
+    currkey.classList.add("pressed");
+  
+    setTimeout(function()
+    {
+         currkey.classList.remove("pressed");
+    },150)
+
+}
+
+
